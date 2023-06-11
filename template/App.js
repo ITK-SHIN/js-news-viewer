882250d4261b1ae75306869d62a250a4d138e1d8
신상우 <bano112@naver.com>
2023-06-12 01:14:56 +0900

    '2개의component로NewsViewer구성완료'

--- template/App.js
+++ template/App.js
@@ -1,1 +1,9 @@
 // do something!
+import nav from './components/Nav.js';
+import newsList from './components/NewsList.js';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+        nav();
+        newsList();
+})
