6b03e1b34f9047cd89e260d271a562bea9b2800b
신상우 <bano112@naver.com>
2023-06-21 15:11:35 +0900

    'API키추가'

--- template/components/NewsList.js
+++ template/components/NewsList.js
@@ -1,16 +1,16 @@
 export default class NewsList {
   constructor(data) {
     this.data = data;
-    this.newsListContainer = document.createElement('div');
-    this.newsListContainer.className = 'news-list-container';
+    this.newsListCon = document.createElement('div');
+    this.newsListCon.className = 'news-list-container';
 
     this.newsListArticle = document.createElement('article');
     this.newsListArticle.className = 'news-list';
     this.newsListArticle.dataset.category = data.category;
-    this.newsListContainer.appendChild(this.newsListArticle);
+    this.newsListCon.appendChild(this.newsListArticle);
 
     this.scrollObserverElement = this.observerElement();
-    this.newsListContainer.appendChild(this.scrollObserverElement);
+    this.newsListCon.appendChild(this.scrollObserverElement);
 
     this.scrollObserver(this.newsListArticle, this.scrollObserverElement);
   }
@@ -22,10 +22,10 @@ export default class NewsList {
     });
   }
 
-  async getNewsList(page = 1, category , pageSize=5) {
+  async getNewsList(page = 1, category, pageSize = 5) {
     const newsArr = [];
-    const NEWS_API_KEY = '4b5fcfae84d64b68ab6bd73c347c8004';
-    const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
+    let NEWS_API_KEY = '4b5fcfae84d64b68ab6bd73c347c8004';
+    let url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
       category === 'all' ? '' : category
     }&page=${page}&pageSize=${pageSize}&apiKey=${NEWS_API_KEY}`;
 
@@ -73,7 +73,58 @@ export default class NewsList {
       });
       return newsArr;
     } catch (error) {
-      // console.log(error);
+      if (error.response && error.response.status === 429) {
+        NEWS_API_KEY = '9d8be12d479a435ab2ec5ac3cfc249b5';
+        url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
+          category === 'all' ? '' : category
+        }&page=${page}&pageSize=${pageSize}&apiKey=${NEWS_API_KEY}`;
+        try {
+          const response = await axios.get(url);
+          const articles = response.data.articles;
+
+          articles.forEach((data) => {
+            if (data.urlToImage === null) {
+              data.urlToImage =
+                'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==';
+            }
+
+            if (data.description === null) {
+              data.description = '';
+            }
+
+            const newsItem = document.createElement('section');
+            newsItem.className = 'news-item';
+            newsItem.insertAdjacentHTML(
+              'beforeend',
+              `
+                      <div class="thumbnail">
+                          <a href=${data.url} target="_blank" 
+                          rel="noopener noreferrer">
+                              <img
+                              src=${data.urlToImage}
+                              alt="thumbnail" />
+                          </a>
+                      </div>
+                      <div class="contents">
+                          <h2>
+                              <a href=${data.url} target="_blank" 
+                              rel="noopener noreferrer">
+                              ${data.title}
+                              </a>
+                          </h2>
+                          <p>
+                          ${data.description}
+                          </p>
+                      </div>
+                  `,
+            );
+            newsArr.push(newsItem);
+          });
+          return newsArr;
+        } catch (error) {
+          return [];
+        }
+      }
       return [];
     }
   }
@@ -118,6 +169,6 @@ export default class NewsList {
   }
 
   get element() {
-    return this.newsListContainer;
+    return this.newsListCon;
   }
 }
